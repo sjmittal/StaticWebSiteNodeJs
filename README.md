ba102e3e1ca9c2ced45f12d388558009acc56797
shujaatali <shujaat.ali012@gmail.com>
2016-05-10 01:24:00 +0500

    list add
    list add with object oriented javascript

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # StaticWebSiteNodeJs
-Simple website using nodejs 
+
+-This is Sample website using nodejs html and and bootstrap
+
+	-Bootstrap is used for responsive design
+
+	-NodeJs is used for server side coding 
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
+/*************************************************/
+/*This http server will handle requests by client*/
+/*************************************************/
+
+/*Global variables required to create http server*/
 var http = require('http');
 var fs = require('fs');
 
+
 /* Create server */
+/* Two parameter required: request and response */
 http.createServer(function(request, response){
 	var url = request.url;
 	switch(url){
 		case '/':
-			getFileContents(response, 'public/home.html', 'text/html');
+			getFileContents(response, 'public/index.html', 'text/html');
 			break;
 		case '/about':
 			getFileContents(response, 'public/about.html', 'text/html');
 			break;
-		/*case '/contact'
+		case '/contact':
 			getFileContents(response, 'public/contact.html', 'text/html');
-			break;*/
+			break;
 		default:
 			response.writeHead(404, {'Content-Type':'text/plan'});
 			response.end('404 - Page not found.');
@@ -21,11 +28,11 @@ http.createServer(function(request, response){
 
 }).listen(9003);
 
-/* message to check server is running or not */
+/* Message for verification of server is running or not */
 console.log("Server is running on port 9003, url = http://localhost:9003");
 
 
-/* function to get contents requested page from file */
+/* Function to get contents requested page from file - Helper method */
 function getFileContents(response, filePath, contentType){
 	fs.readFile(filePath, function(error, data){
 		if(error) {
